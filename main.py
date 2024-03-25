f9a7a72566a85a66b6cd91507b6bcc25b9d91daf
Sai Mahendra <118994059+saimahendra282@users.noreply.github.com>
2024-03-25 12:28:34 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# coding starts 
+import subprocess
+import webbrowser
+import pywhatkit as kit
+import speech_recognition as sr
+import pushbullet
+import timetable
+from gtts import gTTS
+import datetime as dt
+import pywhatkit as pk
+from pydub import AudioSegment
+from pydub.playback import play
+from translate import Translator
+import os
+from playsound import playsound
+# refer to requirement.txt for packages installing.....
